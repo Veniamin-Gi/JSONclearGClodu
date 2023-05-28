756f495fe956451a18692c0ac328b08bb7b98e7e
Veniamin-Gi <golubveniamin41@gmail.com>
2023-05-28 23:59:18 +0300

    sc

--- ClearJSON.py
+++ ClearJSON.py
@@ -3,34 +3,36 @@ import json
 
 def my_filtering_function(pair):
     wanted_keys = ['Id', 'Code']
-    key, value = pair
+    key, _ = pair
     if key in wanted_keys:
-        return True  
+        return True
     else:
-        return False  
- 
-def loadJSON():
-    with open('YourJSON.json', 'r') as file:
-        return json.load(file) 
+        return False
 
-def SaveJSON(data):
+def load_JSON(file_name: str):
+    with open(file_name, 'r') as file:
+        return json.load(file)
+
+def save_JSON(data):
     with open('newJSON.json', 'w') as file:
-        json.dump(data, file)  
+        json.dump(data, file)
 
 # MAIN
-def main():
-    
+def main(file_name: str):
+
     newList = []
-    gradest = loadJSON()
- 
-    for grades in gradest: 
-        newList.append(dict(filter(my_filtering_function, grades.items())))
- 
-    SaveJSON(newList) 
+    gradest = load_JSON(file_name)
+
+    for grades in gradest:
+        newList.append(dict(
+            filter(my_filtering_function, grades.items())
+            ))
+
+    save_JSON(newList)
 
 if __name__ == "__main__":
-    main()
-
+    file_name = sys.argv[1]
+    main(file_name)
 
 
 
